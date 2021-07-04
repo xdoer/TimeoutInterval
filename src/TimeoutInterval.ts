f1eea85b5bdbbac9aa2aef45198eb32c305d8e44
Thomas Trainset <gotoanything@foxmail.com>
2021-07-05 00:21:41 +0800

    refactor: controller

--- src/TimeoutInterval.ts
+++ src/TimeoutInterval.ts
@@ -6,23 +6,18 @@ export class TimeoutInterval {
 
   private timerId: any
   private count = 0
-  private instanceId = 0
-  private delay = 0
-
-  private initDelay() {
-    /**
-     * 由于使用了 `setTimeout` 递归，当第一个注册的 cb interval 比后面注册的 interval 大时，会造成后面执行的时间有误差。
-     * 这里做了取舍，队列中注册的时间大于 1000ms，会默认使用 1000ms 进行递归
-     */
-    this.delay = this.cbs.reduce((min, cur) => cur.interval < min ? cur.interval : min, 1000)
-  }
+  static instanceId = 0
+  delay = 0
+  now = 0
 
   private start() {
+    this.now = Date.now()
     baseSetTimeoutInterval(
       () => {
         for (let i = 0; i < this.cbs.length; i++) {
           const { cb, interval } = this.cbs[i]
           if (!(this.count * this.delay % interval)) {
+            this.now = Date.now()
             cb()
           }
         }
@@ -41,12 +36,13 @@ export class TimeoutInterval {
   }
 
   add(cb: TimerCallBack, interval = 1000) {
-    const id = ++this.instanceId
+    const id = ++TimeoutInterval.instanceId
     this.cbs.push({ cb, interval, id })
 
-    this.initDelay()
-
-    if (!this.timerId) this.start()
+    if (!this.timerId) {
+      this.delay = interval
+      this.start()
+    }
 
     return id
   }
@@ -54,13 +50,15 @@ export class TimeoutInterval {
   remove(id: number) {
     const idx = this.cbs.findIndex(item => item.id === id)
 
-    if (idx !== -1) {
-      this.cbs.splice(idx, 1)
+    if (idx === -1) return false
 
-      if (!this.cbs.length) {
-        this.stop()
-      }
+    this.cbs.splice(idx, 1)
+
+    if (!this.cbs.length) {
+      this.stop()
     }
+
+    return true
   }
 
   removeAll() {
@@ -68,9 +66,3 @@ export class TimeoutInterval {
     this.stop()
   }
 }
-
-export const timeoutInterval = new TimeoutInterval()
-
-export const setTimeoutInterval = timeoutInterval.add.bind(timeoutInterval)
-
-export const clearTimeoutInterval = timeoutInterval.remove.bind(timeoutInterval)
